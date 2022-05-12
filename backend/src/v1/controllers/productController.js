49a455455b8f5a9ee14b2399ea1bb5a2063a197a
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-05-12 15:04:24 +0530

    product database functions added

--- backend/src/v1/controllers/productController.js
+++ backend/src/v1/controllers/productController.js
@@ -1,8 +1,28 @@
-exports.addProductToInventory = function (req, res) {
-    res.send('addProductToInventory_post route');
+const Product = require('../Models/product');
+
+exports.addProductToInventory = function (req, res, next) {
+    const product = new Product({
+        name: req.body.name,
+        img: req.body.img,
+        description: req.body.description,
+        vendor: req.body.vendor,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        reviews: req.body.reviews,
+        rating: req.body.rating,
+        status: req.body.status
+    });
+    product.save(err => {
+        if (err) {
+            return next(err);
+        }
+        res.send('product saved');
+    });
 };
-exports.deleteProductFromInventory = function (req, res) {
-    res.send('deleteProductFromInventory_post route');
+exports.deleteProductFromInventory = function (req, res, next) {
+    Product.findById(req.params.id).deleteOne().then(() => {
+        res.send(`product ${req.params.id} deleted`);
+    });
 };
 exports.modifyProductInInventory = function (req, res) {
     res.send('modifyProductInInventory_post route');
--- backend/src/v1/routes/productRouter.js
+++ backend/src/v1/routes/productRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.post('/addProductToInventory', productController.addProductToInventory);
-router.post('/deleteProductFromInventory', productController.deleteProductFromInventory);
+router.post('/deleteProductFromInventory/:id', productController.deleteProductFromInventory);
 router.post('/modifyProductInInventory', productController.modifyProductInInventory);
 
 module.exports = router;
