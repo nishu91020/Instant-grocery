c2ab39f31add49a9d13dd9ba4fd13ab4abb0bc24
tred7 <ms1514094@gmail.com>
2022-05-12 15:35:32 +0530

    implemented addVendor controller

--- backend/src/v1/controllers/vendorController.js
+++ backend/src/v1/controllers/vendorController.js
@@ -1,14 +1,33 @@
 const Vendor = require("../Models/vendor");
+const crypto = require("crypto");
 
-const addVendor = (req, res, next) => {
-	res.status(200).json({
-		status: "success",
-		data: {
-			vendor: {
-				message: "Vendor is created",
-			},
+function genSalt(size) {
+	return crypto.randomBytes(size).toString("hex");
+}
+
+function generateHash(password, salt) {
+	return crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString("hex");
+}
+
+const addVendor = async (req, res, next) => {
+	const vendorData = {
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: {
+			salt: genSalt(16),
+			hash: generateHash(req.body.password),
 		},
-	});
+	};
+
+	try {
+		const vendor = new Vendor(vendorData);
+		await vendor.save();
+
+		res.status(200).json({
+			status: "success",
+		});
+	} catch (err) {}
 };
 
 module.exports = {
