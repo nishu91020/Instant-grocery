23b294f2aca27b64a0e6f754d5d15a8656ae9fdf
tred7 <ms1514094@gmail.com>
2022-06-10 10:59:34 +0530

    customised app header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-import { ThemeProvider, styled } from '@mui/material/styles';
-import Theme from './Themes';
-import { Typography } from '@mui/material';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import Header from './components/Header';
+import { ThemeProvider, styled } from "@mui/material/styles";
+import Theme from "./Themes";
+import { Box, Typography } from "@mui/material";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Header from "./components/Header";
 
-const CustomParagraph = styled(Typography)(({ theme }) => ({
-    color: theme.palette.primary.dark,
-    transition: theme.transitions.easing.easeInOut,
-    transitionDuration: '800ms',
-    '&:hover': {
-        color: theme.palette.primary.light
-    }
+const StyledRoot = styled(Box)(({ theme }) => ({
+	background: theme.palette.primary.light,
+	height: "100vh",
 }));
 
-function App () {
-    return (
-        <ThemeProvider theme={Theme}>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/signin" element={<Signin />} />
-            </Routes>
-        </ThemeProvider>
-    );
+function App() {
+	return (
+		<ThemeProvider theme={Theme}>
+			<StyledRoot>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/signin" element={<Signin />} />
+				</Routes>
+			</StyledRoot>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import { Typography, AppBar, Toolbar, Button, styled } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import React from "react";
+import { Typography, AppBar, Toolbar, Button, styled } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Link, useNavigate } from "react-router-dom";
 
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
-    background: grey[50],
-    color: theme.palette.primary.dark,
-    display: 'flex',
-    justifyContent: 'center',
-    height: '45px'
+	background: grey[50],
+	color: theme.palette.primary.dark,
+	display: "flex",
+	justifyContent: "center",
+	height: "45px",
+	boxShadow: theme.shadows[2],
 }));
 
 const Header = () => {
-    return (
-        <StyledAppBar position="static">
-            <Toolbar sx={{}}>
-                <Typography variant="h6" fontWeight={600} sx={{ flexGrow: 1 }}>
-                    INSTANT GROCERY
-                </Typography>
-                <Button variant="outlined" size="small" sx={{ margin: '0px 5px' }}>
-                    SIGNUP
-                </Button>
-            </Toolbar>
-        </StyledAppBar>
-    );
+	const navigate = useNavigate();
+
+	return (
+		<StyledAppBar position="static">
+			<Toolbar sx={{}}>
+				<Typography variant="subtitle1" fontWeight={600} sx={{ flexGrow: 1 }}>
+					<Link to="/">Instant Grocery</Link>
+				</Typography>
+				<Button
+					onClick={() => {
+						navigate("/signin");
+					}}>
+					Sign in
+				</Button>
+				<Button
+					variant="contained"
+					size="small"
+					sx={{ margin: "0px 5px" }}
+					onClick={() => {
+						navigate("/signup");
+					}}>
+					Sign up
+				</Button>
+			</Toolbar>
+		</StyledAppBar>
+	);
 };
 
 export default Header;
