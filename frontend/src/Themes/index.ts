a6bfe1758dc6516caff8cb83f845c6b2774a0a9e
tred7 <ms1514094@gmail.com>
2022-06-10 13:25:25 +0530

    added a common home layout

--- frontend/src/Themes/index.ts
+++ frontend/src/Themes/index.ts
@@ -1,30 +1,26 @@
-import '@mui/material/styles';
-import { createTheme, PaletteOptions, responsiveFontSizes } from '@mui/material/styles';
-import { green, grey, orange } from '@mui/material/colors';
-declare module '@mui/material/styles' {
-    // allow configuration using `createTheme`
-    interface ThemeOptions {
-        palette?: PaletteOptions;
-    }
-    interface TypographyOptions {
-        typography?: TypographyOptions;
-    }
+import "@mui/material/styles";
+import { createTheme, PaletteOptions, responsiveFontSizes } from "@mui/material/styles";
+import { green, grey, orange } from "@mui/material/colors";
+declare module "@mui/material/styles" {
+	// allow configuration using `createTheme`
+	interface ThemeOptions {
+		palette?: PaletteOptions;
+	}
 }
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: green[300],
-            light: green[50],
-            dark: green[500],
-            contrastText: grey[900]
-        },
-        secondary: {
-            main: orange[300],
-            light: orange[50],
-            dark: orange[500]
-        }
-    },
-    typography: {}
+	palette: {
+		primary: {
+			main: green[500],
+			light: green[50],
+			dark: green[900],
+			contrastText: grey[50],
+		},
+		secondary: {
+			main: orange[300],
+			light: orange[50],
+			dark: orange[500],
+		},
+	},
 });
 
 let Theme = responsiveFontSizes(theme);
