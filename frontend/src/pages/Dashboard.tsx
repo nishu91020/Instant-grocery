8151725bcdc1daa64374e879976c3a7629ddccba
tred7 <ms1514094@gmail.com>
2022-06-16 02:05:07 +0530

    created product tables

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,38 +1,47 @@
-import React, { useContext } from 'react';
-import StatsCard from '../components/dashboradComponent/StatsCard';
-import VendorProducts from '../components/dashboradComponent/VendorProducts';
-import VendorProfile from '../components/dashboradComponent/VendorProfile';
-import { AuthContext, AuthContextType } from '../context/AuthContext';
-import { Grid, useTheme } from '@mui/material';
+import StatsCard from "../components/dashboradComponent/StatsCard";
+import VendorProducts from "../components/dashboradComponent/VendorProducts";
+import VendorProfile from "../components/dashboradComponent/VendorProfile";
+
+import { Grid, Paper, Typography, useTheme } from "@mui/material";
+import ProductsTable from "../components/dashboradComponent/ProductsTable";
 
 const Dashboard = () => {
-    const Theme = useTheme();
-    const { vendor, isLoading } = useContext(AuthContext) as AuthContextType;
-    return (
-        <Grid container sx={{ padding: `${Theme.spacing(3)}` }} spacing={3} flexDirection="column">
-            <Grid item>
-                <VendorProfile />
-            </Grid>
+	const Theme = useTheme();
+	return (
+		<Grid
+			container
+			sx={{ padding: `${Theme.spacing(3)} ${Theme.spacing(1)}` }}
+			spacing={2}
+			flexDirection="column">
+			<Grid item container>
+				<Grid item flex={1}>
+					<VendorProfile />
+				</Grid>
+			</Grid>
 
-            <Grid container item flexDirection="row" spacing={2} justifyContent="center">
-                <Grid item xs={6} lg={3}>
-                    <StatsCard label="Total Earning" value="12000000" />
-                </Grid>
-                <Grid item xs={6} lg={3}>
-                    <StatsCard label="Total Profit" value="1025006" />
-                </Grid>
-                <Grid item xs={6} lg={3}>
-                    <StatsCard label="Total Items sold" value="1230" />
-                </Grid>
-                <Grid item xs={6} lg={3}>
-                    <StatsCard label="Total Products" value="56000" />
-                </Grid>
-            </Grid>
-            <Grid item>
-                <VendorProducts />
-            </Grid>
-        </Grid>
-    );
+			<Grid container item flexDirection="row" spacing={2} justifyContent="center">
+				<Grid item xs={6} lg={3}>
+					<StatsCard label="Total Earning" value="12000000" />
+				</Grid>
+				<Grid item xs={6} lg={3}>
+					<StatsCard label="Total Profit" value="1025006" />
+				</Grid>
+				<Grid item xs={6} lg={3}>
+					<StatsCard label="Total Items sold" value="1230" />
+				</Grid>
+				<Grid item xs={6} lg={3}>
+					<StatsCard label="Total Products" value="56000" />
+				</Grid>
+			</Grid>
+			<Grid item container>
+				{/* <VendorProducts />
+				 */}
+				<Grid item flex={1} sx={{ overflow: "auto" }}>
+					<ProductsTable />
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Dashboard;
